2d555935c5eba4c9e9a821ed6c0ca9e3095718c8
NoraYu <seeyounora@gmail.com>
2019-09-26 17:09:09 -0400

    add navbar

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,97 @@
+package com.example.demo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import java.io.IOException;
+import java.util.Map;
+
+
+@Controller
+public class HomeController {
+    @Autowired
+    MessagepostRepository messagepostRepository;
+
+    @Autowired
+    CloudinaryConfig cloudc;
+
+    @RequestMapping("/")
+    public String messageList(Model model) {
+        model.addAttribute("messageposts", messagepostRepository.findAll());
+        return "index";}
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+
+
+
+    @GetMapping("/add")
+    public String messageForm(Model model) {
+        model.addAttribute("messagepost", new Messagepost());
+        return "messagepostform";
+    }
+
+
+    @PostMapping("/process")
+    public String processForm(@ModelAttribute Messagepost messagepost, BindingResult result,
+                              @RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()){
+            return  "redirect:/add";
+        }
+        try {
+            Map uploadResult = cloudc.upload(file.getBytes(),
+                    ObjectUtils.asMap("resourcetype","auto"));
+            messagepost.setPic(uploadResult.get("url").toString());
+            messagepostRepository.save(messagepost);
+        }catch (IOException e){
+            e.printStackTrace();
+            return  "redirect:/add";}
+
+        if (result.hasErrors()) {
+            return "messagepostform";
+        }
+        messagepostRepository.save(messagepost);
+        return "redirect:/";
+    }
+    @RequestMapping("/detail/{id}")
+    public String showMessage(@PathVariable("id") long id, Model model){
+        model.addAttribute("messagepost",messagepostRepository.findById(id).get());
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateItem(@PathVariable("id") long id, Model model){
+        model.addAttribute("messagepost",messagepostRepository.findById(id).get());
+        return "messagepostform";}
+
+    @RequestMapping("/delete/{id}")
+    public String deleteItem(@PathVariable("id") long id){
+        messagepostRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/author")
+    public String authorPage(){
+        return "author";
+    }
+
+    @RequestMapping("/about")
+    public String aboutPage(){
+        return "about";
+    }
+
+    @RequestMapping("/videos")
+    public String videoPage(){
+        return "videos";
+    }
+}
---
+++ src/main/java/com/example/demo/Messagepost.java
@@ -0,0 +1,66 @@
+package com.example.demo;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+public class Messagepost {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @NotNull
+    private String title;
+    @NotNull
+    private String content;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date posteddate;
+
+    private String pic;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getPosteddate() {
+        return posteddate;
+    }
+
+    public void setPosteddate(Date posteddate) {
+        this.posteddate = posteddate;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+}
